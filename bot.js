de7834ca6504cb1b6df3d6340457b2dc979536df
Bahaa <bahaa.bakri1995@gmail.com>
2024-10-18 17:02:53 +0400

    remove bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,16 @@
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf('7505265606:AAHw6PHnNAb2kX9kPLJYvgaypsROWuf-f6c'); // Replace with your bot token from BotFather
+// const { Telegraf } = require('telegraf');
+// const bot = new Telegraf('7505265606:AAHw6PHnNAb2kX9kPLJYvgaypsROWuf-f6c'); // Replace with your bot token from BotFather
+// let chatId
+// bot.start((ctx) => {
+//     console.log(ctx);
+//     chatId = ctx.chat.id
+//     ctx.reply('Welcome to Blove Dapp! Click the button below to open the Web App.', {
+//         reply_markup: {
+//             inline_keyboard: [
+//                 [{ text: 'Open Web App', web_app: { url: 'https://blove-dapp.vercel.app' } }] // Replace with your web app URL
+//             ]
+//         }
+//     });
+// });
 
-bot.start((ctx) => {
-    ctx.reply('Welcome! Click the button below to open the Web App.', {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Open Web App', web_app: { url: 'https://blove-dapp.vercel.app' } }] // Replace with your web app URL
-            ]
-        }
-    });
-});
-
-bot.launch();
+// bot.launch();
